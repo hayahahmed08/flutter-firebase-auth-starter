41412e3d9fe55f095797913fa5d59c475dd6e218
hayahahmed08 <hayahahmeduser2@gmail.com>
2025-01-08 21:43:11 +0500

    Update login_screen.dart

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -25,98 +24,110 @@ class _LoginScreenState extends State<LoginScreen> {
     emailController.dispose();
     passwordController.dispose();
   }
-void login(){
 
-    _auth.signInWithEmailAndPassword(email: emailController.text.toString(), password: passwordController.text.toString()).then((value){
-
-    }).onError((error,stackTrace){
+  void login() {
+    _auth
+        .signInWithEmailAndPassword(
+            email: emailController.text.toString(),
+            password: passwordController.text.toString())
+        .then((value) {
+          Utils().toastMessage(value.user!.email.toString());
+    })
+        .onError((error, stackTrace) {
+          debugPrint(error.toString());
       Utils().toastMessage(error.toString());
     });
-}
-  Widget build(BuildContext context) {
-    return WillPopScope(onWillPop: ()async{
-      SystemNavigator.pop();
-      return true;
-    },child:  SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.deepPurple,
-          title: const Center(
-              child: Text(
-            'Login ',
-            style: TextStyle(color: Colors.white),
-          )),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                          hintText: 'Email',
-                          helperText: '  abc@example.com',
-                          prefixIcon: Icon(Icons.alternate_email)),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter email';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        prefixIcon: Icon(Icons.lock_open),
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter password';
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              RoundButton(
-                title: 'login',
-                onTap: () {
-                  if (_formKey.currentState!.validate()) {
-                    login();
-                  }
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have an account?"),
-                  TextButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => SignupScreen()));
+  }
 
-                  }, child: Text('Sign up'))
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async {
+          SystemNavigator.pop();
+          return true;
+        },
+        child: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.deepPurple,
+              title: const Center(
+                  child: Text(
+                'Login ',
+                style: TextStyle(color: Colors.white),
+              )),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                              hintText: 'Email',
+                              helperText: '  abc@example.com',
+                              prefixIcon: Icon(Icons.alternate_email)),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Enter email';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            prefixIcon: Icon(Icons.lock_open),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Enter password';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RoundButton(
+                    title: 'login',
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        login();
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an account?"),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignupScreen()));
+                          },
+                          child: Text('Sign up'))
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        ),
-      ),
-    )
-    );
+        ));
   }
 }
